614f39567b81d6f0a364fe9b784ffcaed1b5267f
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-06 14:55:31 -0500

    finished map functions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const mapToNegativize = (sourceArray) => {
+  const results = [...sourceArray]
+  for (let i = 0; i < results.length; i++) {
+    results[i] = results[i] * -1
+  }
+  return results
+} 
+
+const mapToNoChange =  (sourceArray) => {
+  const results = [...sourceArray]
+  for (let i = 0; i < results.length; i++) {
+    results[i] = results[i]
+  }
+  return results
+}
+
+const mapToDouble = (sourceArray) => {
+  const results = [...sourceArray]
+  for (let i = 0; i < results.length; i++) {
+    results[i] = results[i] * 2
+  }
+  return results
+}
+
+const mapToSquare = (sourceArray) => {
+  const results = [...sourceArray]
+  for (let i = 0; i < results.length; i++) {
+    results[i] = results[i] * results[i]
+  }
+  return results
+}
